2af27edfa776e90ff98041151ab4e60dffa00d9c
sidney87812 <sidney87812@gmil.com>
2017-03-19 22:31:41 +0800

    lab1B

---
+++ lab1B.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<< "Enter a number"<< endl;
+cin >> n ;
+cout << n << endl;
+
+while(n!=1){
+
+if(n%2==1){
+n=3*n+1;}
+
+else{
+n=n/2;}
+
+cout << n << endl;
+}
+
+return 0;
+}
